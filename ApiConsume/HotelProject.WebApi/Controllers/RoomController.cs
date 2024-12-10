d18485c3e7cc8791ffa6876734a00e56b5fc3470
edaagunes <edagunes00@hotmail.com>
2024-12-10 23:59:19 +0300

    Added Room transactions in admin room

--- ApiConsume/HotelProject.WebApi/Controllers/RoomController.cs
+++ ApiConsume/HotelProject.WebApi/Controllers/RoomController.cs
@@ -30,7 +30,7 @@ namespace HotelProject.WebApi.Controllers
 			return Ok("Ekleme Başarılı");
 		}
 
-		[HttpDelete]
+		[HttpDelete("{id}")]
 		public IActionResult DeleteRoom(int id)
 		{
 			var values = _roomService.TGetById(id);
